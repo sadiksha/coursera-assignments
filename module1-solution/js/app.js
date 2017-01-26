bd64bea27a990baa439e1d9ceb4582f8ac98b87d
Sadiksha Gautam <sadiksha.gautam@ericsson.com>
2017-01-26 22:43:48 +0100

    Adds message when the input field is empty

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -4,16 +4,21 @@
   angular.module('LunchCheck', [])
   .controller('LunchCheckController', function($scope) {
     $scope.displayMessage = function() {
-      var message = splitTextAndPrintMessage($scope.lunch_menu);
+      var message;
+      if($scope.lunch_menu == undefined) {
+        message = "Please enter data first";
+      } else {
+        message = splitTextAndPrintMessage($scope.lunch_menu);
+      }
       $scope.message = message;
     }
 
     function splitTextAndPrintMessage(string) {
       var split_array = string.split(",");
       if(split_array.length <= 3) {
-          return "Enjoy!";
-      } else {
-        return "Too much!"
+        return "Enjoy!";
+      }else {
+        return "Too much!";
       }
     }
   })
