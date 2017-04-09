995a76832987019e7fbaa8d2481a36c686f3b18f
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2017-04-09 17:32:49 +0200

    [Module2] Changes the message when to buy or bought items are empty.

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,32 +1,38 @@
 (function () {
   'use strict';
 
-  var toBuy = [{
-                name: "cookies",
-                quantity: 10
-              },
-              {
-                name: "chocolates",
-                quantity: 5
-              },
-              {
-                name: "chips",
-                quantity: 3
-              },
-              {
-                name: "avocado",
-                quantity: 2
-              },
-              {
-                name: "healthy snack",
-                quantity: 5
-              }]
+  var toBuyItems = [{
+                     name: "cookies",
+                     quantity: 10
+                   },
+                   {
+                     name: "chocolates",
+                     quantity: 5
+                   },
+                   {
+                     name: "chips",
+                     quantity: 3
+                   },
+                   {
+                     name: "avocado",
+                     quantity: 2
+                   },
+                   {
+                     name: "healthy snack",
+                     quantity: 5
+                   }]
 
   angular.module('ShoppingListCheckOff', [])
-  .controller('ToBuyController', ToBuyController);
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController);
 
   ToBuyController.$inject = ['$scope'];
   function ToBuyController($scope) {
-    $scope.toBuy = toBuy;
+    $scope.toBuyItems = toBuyItems;
+  }
+
+  AlreadyBoughtController.$inject = ['$scope'];
+  function AlreadyBoughtController($scope) {
+    $scope.boughtItems = [];
   }
 })();
